5d03171ea78bcdec0ec7c48719c817f83f1c7b84
Anu A <167222325+anu-2024@users.noreply.github.com>
2026-06-24 13:11:40 +0530

    Update history.py

--- pages/history.py
+++ pages/history.py
@@ -1,1 +1,52 @@
 # history page placeholder
+import streamlit as st
+import sqlite3
+
+
+DB_NAME = "interview_agent.db"
+
+
+def show_history():
+
+    st.title(
+        "📜 Interview History"
+    )
+
+    try:
+
+        conn = sqlite3.connect(
+            DB_NAME
+        )
+
+        query = """
+        SELECT
+        id,
+        final_score,
+        percentage,
+        created_at
+        FROM scores
+        """
+
+        history = conn.execute(
+            query
+        ).fetchall()
+
+        conn.close()
+
+        if len(history) == 0:
+
+            st.info(
+                "No History Available"
+            )
+
+        else:
+
+            st.dataframe(
+                history
+            )
+
+    except Exception as e:
+
+        st.error(
+            str(e)
+        )
